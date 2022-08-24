58ee162e8793fdacef019fa67c2129194a4ac5fc
Panagiotis Fatouros <86960265+PanagiotisFatouros@users.noreply.github.com>
2022-08-24 14:36:34 +1000

    Update pipeline to run on PR to main branch. (#6)
    * Create GitHub Actions to run tests and linter as well as build app.
    
    * IT-20 : Break the CI so it doesn't run for the moment.
    
    * Update gitignore and fix pipeline to run on PR to branch
    
    * Add babel eslint-parser to linter.
    
    * Linter to now target all .js files and file types specified in override.
    
    * Remove test for old webpage that used to exist.
    
    * Add an empty test so test runner has something to do while we don't have any of our own tests.
    
    * Install playwright before running tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,17 @@
 module.exports = {
 	root: true,
 	extends: ['eslint:recommended', 'prettier'],
+	rules: {
+		// enable additional rules
+		indent: ["error", 4],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+	},
 	plugins: ['svelte3'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	parser: "@babel/eslint-parser",
 	parserOptions: {
+		requireConfigFile: false,
 		sourceType: 'module',
 		ecmaVersion: 2020
 	},
