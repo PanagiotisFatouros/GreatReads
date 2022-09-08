a01f5dc4cadf1a57f0923b9d99784c53d208df99
lucasvantilburg <78183150+lucasvantilburg@users.noreply.github.com>
2022-09-08 14:34:41 +1000

    It 25 (#9)
    * implemented search bar
    
    * created star rating component, added custom font sizes to the tailwind config file
    
    * Update pipeline and ESLint to use Svelte and Typscript together correctly.
    
    Co-authored-by: Panagiotis Fatouros <pfatouros@student.unimelb.edu.au>
    Co-authored-by: Panagiotis Fatouros <86960265+PanagiotisFatouros@users.noreply.github.com>

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -1,18 +1,36 @@
 <script>
+    let searchTypes = ["Books", "Users", "Genres", "Authors"];
+    let selected = searchTypes[0];
+    let searchText = "";
+
+    function handleSubmit() {
+        //TODO: test for empty search text
+        
+        alert(`type: ${selected}, searchText: ${searchText}`)
+    }
 </script>
 
-<input id="searchBar" type="text" class=" w-64 bg-primary-1 rounded-3xl h-3/6 self-center border-none mx-2">
+<form on:submit|preventDefault={handleSubmit} class="flex content-center justify-center h-1/2 self-center rounded-full overflow-hidden font-body text-primary-3 text-body1">
+    <div class=" bg-primary-1 flex">
+        <select bind:value={selected} class=" bg-transparent w-full pl-2 cursor-pointer focus:outline-none">
+            {#each searchTypes as searchType}
+            <option value={searchType}>
+                {searchType}
+            </option>
+            {/each}
+        </select>
+    </div>
 
-<style>
-    /* #searchBar {
-        width: 250px;
-        background-color: white;
-        border-radius: 20px;
-        height: 50%;
-        align-self: center;
-        border: none;
-        margin-left: 10px;
-        margin-right: 10px;
-    } */
-
-</style>
+    <div class=" px-2 bg-white">
+        <input type="text" bind:value={searchText} class=" h-full focus:outline-none">
+    </div>
+    
+    <div class="bg-white">
+        <button type="submit" class=" w-full h-full pr-2 py-1">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full h-full text-primary-3">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+              </svg>              
+        </button>
+    </div>
+    
+</form>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
+<script>
+    //TODO: remove
+	import StarRating from '../components/StarRating.svelte';
+</script>
 
+<StarRating rating={4.3} />
+<StarRating rating={1.0} />
+<StarRating rating={2.7} />
+<StarRating rating={0.5} />
+<StarRating rating={4.9} />
+<StarRating rating={4.8} />
 
