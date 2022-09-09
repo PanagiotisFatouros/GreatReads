bcd0fc8cbce1793efd98d3870328fe9ec87ed605
Luhuala <nixi@student.unimelb.edu.au>
2022-09-09 14:39:02 +1000

    Added bookcard structure

--- src/components/BookCard.svelte
+++ src/components/BookCard.svelte
@@ -1,14 +1,21 @@
 <script>
+    let bookcard = {
+        name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        author: "Author",
+        rating: "",
+        reviewNum: "",
+        genres: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt quam ac diam scelerisque, in convallis odio pellentesque. Pellentesque ac justo nisi. Praesent aliquet consequat convallis. Sed quis enim non libero aliquam bibendum."
+    }
 </script>
 
 <div id="bookCard" class="bg-primary-1 w-3/12 h-52 m-4 p-2 pr-4 rounded-lg">
-    <a href="null"> <!-- links to book page -->
+    <a href="null">
         <img src="src/TestBook.jpg" alt="Book" class="p-3 pr-4 w-34 float-left align-middle">
-        <p class="text-secondary pt-3 font-heading text-xl font-bold truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-        <p class="text-primary-3 font-body text-sm">Author</p>
-        <p class="text-primary-3 py-2 font-body text-sm">Reviews</p>
-        <p class="text-black font-body text-sm line-clamp-2 overflow-hidden"><span class="text-secondary">Genres: </span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt quam ac diam scelerisque, in convallis odio pellentesque. Pellentesque ac justo nisi. Praesent aliquet consequat convallis. Sed quis enim non libero aliquam bibendum.</p>
+        <p class="text-secondary pt-3 font-heading text-xl font-bold truncate">{bookcard.name}</p>
+        <p class="text-primary-3 font-body text-sm">{bookcard.author}</p>
+        <!-- star rating here -->
+        <p class="text-primary-3 py-2 font-body text-sm">{bookcard.reviewNum} Reviews</p>
+        <p class="text-black font-body text-sm line-clamp-2 overflow-hidden"><span class="text-secondary">Genres: </span>{bookcard.genres}</p>
     </a>
     <button href="null" class="bg-secondary w-32 h-7 rounded-full mt-2 text-white font-body">+ Save Book</button>
 </div>
