0c85810f50d0956fefc35be41d21d60318c0e7ee
Luhuala <nixi@student.unimelb.edu.au>
2022-09-09 14:52:57 +1000

    Added max-height for img

--- src/components/BookCard.svelte
+++ src/components/BookCard.svelte
@@ -1,5 +1,6 @@
 <script>
     let bookcard = {
+        img: "http://books.google.com/books/content?id=wDVV6y-8YHEC&printsec=frontcover&img=1&zoom=1&source=gbs_api",
         name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
         author: "Author",
         rating: "",
@@ -10,12 +11,12 @@
 
 <div id="bookCard" class="bg-primary-1 w-3/12 h-52 m-4 p-2 pr-4 rounded-lg">
     <a href="null">
-        <img src="src/TestBook.jpg" alt="Book" class="p-3 pr-4 w-34 float-left align-middle">
+        <img src={bookcard.img} alt="Book" class="p-3 pr-4 w-34 max-h-48 float-left align-middle">
         <p class="text-secondary pt-3 font-heading text-xl font-bold truncate">{bookcard.name}</p>
         <p class="text-primary-3 font-body text-sm">{bookcard.author}</p>
         <!-- star rating here -->
         <p class="text-primary-3 py-2 font-body text-sm">{bookcard.reviewNum} Reviews</p>
         <p class="text-black font-body text-sm line-clamp-2 overflow-hidden"><span class="text-secondary">Genres: </span>{bookcard.genres}</p>
     </a>
-    <button href="null" class="bg-secondary w-32 h-7 rounded-full mt-2 text-white font-body">+ Save Book</button>
+    <button href="null" class="bg-secondary w-32 h-7 rounded-full mt-3 text-white font-body">+ Save Book</button>
 </div>
