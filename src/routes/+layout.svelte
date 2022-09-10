8e15526f18b0a8bb3736d0bca095903a941c8cd2
yuqi-zheng <yuqizheng02@gmail.com>
2022-09-10 17:51:08 +1000

    (850px wide) Long Books display component added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
 	import '../app.css';
 	import NavBar from '../components/NavBar.svelte';
+	import BooksDisplay from '../components/BooksDisplay.svelte';
 	
 </script>
 
 <NavBar />
+<BooksDisplay/>
 
 <slot />
