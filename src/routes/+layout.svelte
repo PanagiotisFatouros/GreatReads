7291922ced92b5d99633df43b82e45b0e16db06b
yuqi-zheng <yuqizheng02@gmail.com>
2022-09-10 18:00:23 +1000

    removed test component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,9 @@
 <script>
 	import '../app.css';
 	import NavBar from '../components/NavBar.svelte';
-	import BooksDisplay from '../components/BooksDisplay.svelte';
 	
 </script>
 
 <NavBar />
-<BooksDisplay/>
 
 <slot />
