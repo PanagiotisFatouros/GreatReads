35e071ac23bcd4e3a6977b446f86365788b6461c
yuqi-zheng <yuqizheng02@gmail.com>
2022-09-19 22:56:33 +1000

    made statistics component

---
+++ src/components/ProfileStatistics.svelte
@@ -0,0 +1,30 @@
+<script>
+    let person = {
+        booksRead: 45,
+        numReviews: 53,
+        averageRating: 3.9,
+        favAuthor: "Suzanne Collins",
+        favGenre: "Crime"
+    }
+
+</script>
+
+<div id="card" class="bg-primary-1">
+    <p class="font-body text-primary-3">Books Read: <span class="text-primary-2">{person.booksRead}</span></p>
+    <p class="font-body text-primary-3">Number of Reviews: <span class="text-primary-2">{person.numReviews}</span></p>
+    <p class="font-body text-primary-3">Average Rating: <span class="text-primary-2">{person.averageRating}</span></p>
+    <p class="font-body text-primary-3">Favourite Author: <span class="text-primary-2">{person.favAuthor}</span></p>
+    <p class="font-body text-primary-3">Favourite Genre: <span class="text-primary-2">{person.favGenre}</span></p>
+</div>
+
+<style>
+    #card {
+        display: flex;
+        flex-direction: column;
+        border-radius: 15px;
+        width: 350px;
+        padding: 10px;
+        overflow-x: auto;
+        white-space: nowrap;
+    }
+</style>
---
+++ src/routes/profile/[slug]/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import BooksDisplay from '../../../components/BooksDisplay.svelte';
+    import ProfileStatistics from '../../../components/ProfileStatistics.svelte';
+</script>
+
+<ProfileStatistics/>
+
+<style>
+
+</style>
