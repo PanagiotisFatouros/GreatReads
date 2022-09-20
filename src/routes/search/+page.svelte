917500ac60681ff7c0a6691db327ea6ecfc49df3
Luhuala <nixi@student.unimelb.edu.au>
2022-09-20 16:49:28 +1000

    Created basic layout for search page
    Also edited BookCard width

---
+++ src/routes/search/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import BookCard from '../../components/BookCard.svelte';
+
+    let searchTerm:string = ""
+</script>
+
+<div class="mt-6 mx-8">
+    <div class="text-primary-3 text-heading2 font-heading">Search Results for "{searchTerm}"</div>
+    <hr class=" border-1 border-primary-3 my-3">
+    <div class="text-primary-3 text-heading3 font-heading flex">
+        <div style="cursor:pointer" class="flex">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6 text-primary-3">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z" />
+            </svg>
+            <p class="ml-1 mr-3">Filter</p>
+        </div>
+        <div style="cursor:pointer" class="flex">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6 text-primary-3">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 6.878V6a2.25 2.25 0 012.25-2.25h7.5A2.25 2.25 0 0118 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 004.5 9v.878m13.5-3A2.25 2.25 0 0119.5 9v.878m0 0a2.246 2.246 0 00-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0121 12v6a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18v-6c0-.98.626-1.813 1.5-2.122" />
+            </svg>
+            <p class="ml-1 mr-3">Sort</p>
+        </div>
+    </div>
+</div>    
+
+<div class="mx-7 flex flex-row flex-wrap grow justify-items-center items-center">
+    <BookCard/>
+    <BookCard/>
+    <BookCard/>
+    <BookCard/>
+    <BookCard/>
+</div>
