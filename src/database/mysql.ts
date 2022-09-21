73baa64ec73818b541b14106339f5e8855cc71f6
Panagiotis Fatouros <pfatouros@student.unimelb.edu.au>
2022-09-21 12:23:33 +1000

    Update package to have "start" as a npm command and deployment to run on push to main branch.

--- src/database/mysql.ts
+++ src/database/mysql.ts
@@ -1,38 +1,38 @@
 import mysql from 'mysql2/promise';
 
-export const mysqlconn = await mysql.createConnection({ 
+export const mysqlconn = await mysql.createConnection({
     host: 'great-reads.mysql.database.azure.com',
     user: 'team0',
-    password: 'EvXg91q6%Xc0',
+    password: 'EvXg91q6%Xc0', // Hide this
     database: 'testdb'
 });
 
 // @ts-ignore
-export async function createNewEntity(table, fields, values){
+export async function createNewEntity(table, fields, values) {
     let createdEntity
-    const queryString =  `INSERT INTO ${table} ${fields} Values ${values}` 
+    const queryString = `INSERT INTO ${table} ${fields} Values ${values}`
     await mysqlconn.query(queryString)
         // @ts-ignore
-        .then(function([rows,fields]) {
+        .then(function ([rows, fields]) {
             // console.log(rows);
             let rowArray = JSON.parse(JSON.stringify(rows));
             createdEntity = rowArray.length == 0 ? null : rowArray[0];
         }).catch((err) => console.log(err));
-    
-    if (typeof createdEntity == undefined){
+
+    if (typeof createdEntity == undefined) {
         return null;
     }
-    else{
+    else {
         console.log(createdEntity)
     }
     return createdEntity;
 }
 
 // @ts-ignore
-export async function retrieveOneEntity(table, field, value){
+export async function retrieveOneEntity(table, field, value) {
     let queryString = `SELECT * from ${table} where ${field} = '${value}';`
     let targetEntity = await mysqlconn.query(queryString)
-        .then(function([rows,fields]) {
+        .then(function ([rows, fields]) {
             // console.log(rows);
             let rowArray = JSON.parse(JSON.stringify(rows));
             return rowArray.length == 0 ? null : rowArray[0];
@@ -41,9 +41,9 @@ export async function retrieveOneEntity(table, field, value){
 }
 
 // @ts-ignore
-export async function getAllRows(table, conditions?, values?){
+export async function getAllRows(table, conditions?, values?) {
     let queryString: string = `SELECT * from ${table}`
-    if (typeof conditions !== 'undefined' && typeof values !== 'undefined'){
+    if (typeof conditions !== 'undefined' && typeof values !== 'undefined') {
         queryString += " where"
         let firstCondition = conditions.at(0)
         conditions.shift()
@@ -52,13 +52,13 @@ export async function getAllRows(table, conditions?, values?){
         let conditionalString: string = ` ${firstCondition} = ${firstValue}`
 
 
-        if (conditions?.length == values?.length){
+        if (conditions?.length == values?.length) {
             conditions.forEach((nextCondition, index) => {
                 let nextValue = values.at(index)
                 conditionalString += ` and ${nextCondition} = ${nextValue}`
             })
         }
-        else{
+        else {
             console.log("conditions or values array are empty")
         }
         queryString += conditionalString
@@ -67,11 +67,11 @@ export async function getAllRows(table, conditions?, values?){
     }
 
     let targetRows = await mysqlconn.query(queryString)
-    .then(function([rows,fields]) {
-        // console.log(rows);
-        let rowArray = JSON.parse(JSON.stringify(rows));
-        return rowArray
-    }).catch((err) => console.log(err));
+        .then(function ([rows, fields]) {
+            // console.log(rows);
+            let rowArray = JSON.parse(JSON.stringify(rows));
+            return rowArray
+        }).catch((err) => console.log(err));
 
     console.log("testSQL")
     console.log(targetRows)
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,9 +8,9 @@ const config = {
 	},
 	preprocess: [
 		preprocess({
-		  postcss: true,
+			postcss: true,
 		}),
-	  ],
+	],
 };
 
 export default config;
