bc1c59c86825c46cd3bfe9816d0e79f62f8ddc53
Luhuala <88075053+Luhuala@users.noreply.github.com>
2022-09-22 13:05:20 +1000

    It 36 Create search results page (#26)
    * Created basic layout for search page
    
    Also edited BookCard width
    
    * Now displays each book in array

--- src/components/BookCard.svelte
+++ src/components/BookCard.svelte
@@ -1,5 +1,5 @@
 <script>
-    let bookcard = {
+    export let bookcard = {
         img: "http://books.google.com/books/content?id=wDVV6y-8YHEC&printsec=frontcover&img=1&zoom=1&source=gbs_api",
         name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
         author: "Author",
@@ -9,7 +9,7 @@
     }
 </script>
 
-<div id="bookCard" class="bg-primary-1 w-3/12 h-52 m-4 p-2 pr-4 rounded-lg">
+<div id="bookCard" class="bg-primary-1 w-114 h-52 m-4 p-2 pr-4 rounded-lg">
     <a href="null">
         <img src={bookcard.img} alt="Book" class="p-3 pr-4 w-34 max-h-48 float-left align-middle">
         <p class="text-secondary pt-3 font-heading text-xl font-bold truncate">{bookcard.name}</p>
