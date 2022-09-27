86edf551d4a93e0606df7d91739ef5adf09b5941
PanagiotisFatouros <pfatouros@student.unimelb.edu.au>
2022-09-27 15:39:59 +1000

    Remove old test and redo get-book-api tests.

--- tests/get-book-api.test.js
+++ tests/get-book-api.test.js
@@ -10,12 +10,12 @@ const testUser= {
 
 test('Valid Book', async ({ page, baseURL }) => {
 	const validBookId = "zyTCAlFPjgYC"
-	const response = await page.goto(`${baseURL}/api/read/books/${validBookId}/${testUser.id}`);
+	const response = await page.goto(`localhost:3000/api/read/books/${validBookId}/${testUser.id}`);
 	await expect(response?.ok()).toBeTruthy()
 });
 
 test('Invalid Book - Book Landing Page', async({ page, baseURL }) => {
 	const invalidBookId = "nosuchbook12"
-	const response = await page.goto(`${baseURL}/api/read/books/${invalidBookId}}/${testUser.id}`);
-	await expect(!(response?.ok())).toBeTruthy()
+	const response = await page.goto(`localhost:3000/api/read/books/${invalidBookId}}/${testUser.id}`);
+	await expect(response?.ok()).toBeFalsy()
 })
