eb2ceaf65cd16843125a9d4affd3a96533282ce8
PanagiotisFatouros <pfatouros@student.unimelb.edu.au>
2022-09-27 17:57:22 +1000

    Add code coverage check.

--- playwright.config.js
+++ playwright.config.js
@@ -1,8 +1,11 @@
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
+	testDir: 'tests',
+	timeout: 3000,
 	webServer: {
 		command: 'npm run preview',
-		port: 3000
+		port: 3000,
+		reuseExistingServer: !process.env.CI,
 	}
 };
 
