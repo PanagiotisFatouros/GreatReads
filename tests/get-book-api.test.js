ba338a911b2d09aae047b738ce5e2abe8a17a31a
haoens <seehaoen88@gmail.com>
2022-09-28 15:03:35 +1000

    update test and config

--- tests/get-book-api.test.js
+++ tests/get-book-api.test.js
@@ -8,14 +8,19 @@ const testUser= {
 	bio: ""
 }
 
-test('Valid Book', async ({ page, baseURL }) => {
-	const validBookId = "zyTCAlFPjgYC"
-	const response = await page.goto(`${baseURL}/api/read/books/${validBookId}/${testUser.id}`);
-	await expect(response?.ok()).toBeTruthy()
+// no such book exists in our database or in Google Books
+const invalidBookId = "nosuchbook12"
+const validBookId = "zyTCAlFPjgYC"
+
+test('Valid Book - GET Request', async ({ request, baseURL }) => {
+    // console.log(baseURL)
+	const response = await request.get(`${baseURL}/api/read/books/${validBookId}/${testUser.id}`);
+	await expect(response.ok()).toBeTruthy()
 });
 
-test('Invalid Book - Book Landing Page', async({ page, baseURL }) => {
-	const invalidBookId = "nosuchbook12"
-	const response = await page.goto(`${baseURL}/api/read/books/${invalidBookId}}/${testUser.id}`);
-	await expect(response?.ok()).toBeFalsy()
-})
+test('Invalid Book - GET Request', async ({ request, baseURL }) => {
+    // console.log(baseURL)
+	const response = await request.get(`${baseURL}/api/read/books/${invalidBookId}/${testUser.id}`);
+	await expect(response.ok()).toBeFalsy()
+});
+
