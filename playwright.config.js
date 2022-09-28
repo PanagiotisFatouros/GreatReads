214ba69697b8d578ccda1a9289c8864e8b9b3049
PanagiotisFatouros <pfatouros@student.unimelb.edu.au>
2022-09-28 16:07:59 +1000

    Update timeout and base url

--- playwright.config.js
+++ playwright.config.js
@@ -1,12 +1,13 @@
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
 	testDir: 'tests',
-	timeout: 3000,
+	timeout: 6000,
 	webServer: {
 		command: 'npm run preview',
 		port: 3000,
 		reuseExistingServer: !process.env.CI,
-	}
+	},
+	baseURL: "http://127.0.0.1:3000/"
 };
 
 export default config;
