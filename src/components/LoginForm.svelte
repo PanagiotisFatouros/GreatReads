19806f364b8e3d4140c1b528251f15659dc4b60d
yuqi-zheng <yuqizheng02@gmail.com>
2022-09-30 23:20:40 +1000

    improved styling

--- src/components/LoginForm.svelte
+++ src/components/LoginForm.svelte
@@ -14,13 +14,13 @@
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z" />
             </svg>          
-            <input class="bg-primary-1" name="username" type="username" placeholder="Email"> 
+            <input class="bg-primary-1 font-heading" name="username" type="username" placeholder="Email"> 
         </div>
         <div class="input bg-primary-1">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
             </svg>  
-            <input class="bg-primary-1" name="password" type="password" placeholder="Password">
+            <input class="bg-primary-1 font-heading" name="password" type="password" placeholder="Password">
         </div>
         <div id="buttons">
             <button on:click={closeOverlay} class="bg-primary-3 text-primary-1">Cancel</button>
