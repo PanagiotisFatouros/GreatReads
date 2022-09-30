93b7620de068cb55caafa801b1b6528ece7101f1
yuqi-zheng <yuqizheng02@gmail.com>
2022-09-30 22:42:06 +1000

    added functionality to login from register page

--- src/routes/authentication/register/+page.svelte
+++ src/routes/authentication/register/+page.svelte
@@ -1,10 +1,16 @@
 <script>
-
+    import LoginForm from "../../../components/LoginForm.svelte";
+    import { isOverlayOpen } from "../../../stores/OverlayStore";
 </script>
+
+{#if $isOverlayOpen}
+    <LoginForm/>
+{/if}
+
 
 <div id="panel">
     <form formaction="?/register">
-        <p id="title" class="text-heading1 font-heading text-secondary">Register an Account</p>
+        <h1 id="title" class="text-heading1 font-heading text-secondary">Register an Account</h1>
         <div class="input-container bg-primary-1">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
