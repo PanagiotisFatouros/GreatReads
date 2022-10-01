46b8a18ab33e14d545cfd683cb2ab416847ad2e6
lucasvantilburg <lucasvantilburg101202@gmail.com>
2022-10-01 22:45:33 +1000

    created ui for editing collection, refactored collection input

--- src/components/BookPageComponents/Notes/NotesCollection.svelte
+++ src/components/BookPageComponents/Notes/NotesCollection.svelte
@@ -7,30 +7,14 @@
 	import NoteInput from './NoteInput.svelte';
 
 	import { page } from '$app/stores';
+	import CollectionInput from './CollectionInput.svelte';
 
 	const baseURL = $page.url.origin;
-
-	onMount(() => {
-		if (collection != null) {
-			isPublic = collection.isPublic;
-		} else {
-			isPublic = false;
-		}
-	});
 
 	const dispatch = createEventDispatcher();
 
 	//selected collection
 	export let collection: Collection | null;
-
-	let isPublic: boolean;
-
-	//runs everytime isPublic value changes
-	$: if (isPublic != undefined) {
-		// TODO: save change
-		collection!.isPublic = isPublic;
-
-	}
 
 	// keep track of which confirmation message to show
 	let deletingCollection: boolean = false;
@@ -102,10 +86,18 @@
 		}
 	}
 
+	let editingCollection: boolean = false;
+
+	function showEditInput() {
+		editingCollection = true;
+		isOverlayOpen.set(true);
+	}
+
 	function triggerRefresh() {
 		console.log("triggering refresh");
 		if (collection != null && collection.notes != undefined) {
-			collection.notes = collection.notes;
+			//collection.notes = collection.notes;
+			collection = collection;
 		}
 	}
 
@@ -127,6 +119,8 @@
 		on:cancel={cancelDeleteNote}
 		on:confirm={deleteNote}
 	/>
+{:else if $isOverlayOpen && editingCollection && collection != null}
+	<CollectionInput collection={collection} on:cancel={() => editingCollection = false} on:update={triggerRefresh}/>
 {/if}
 
 <div class="flex flex-col">
@@ -152,23 +146,34 @@
 		</div>
 
 		<div class="flex items-center space-x-3">
-			<div class="mr-2">
-				<label for="publicCheckbox" class="text-body1 font-body mr-1">Make Public</label>
-				<!-- TODO: replace with toggle -->
-				<input
-					type="checkbox"
-					id="publicCheckbox"
-					bind:checked={isPublic}
-					class=" text-accent rounded-sm"
-				/>
+			<!-- isPublic display -->
+			<div class="mr-2 flex space-x-1">
+				{#if collection?.isPublic}
+				<p>Public Collection</p>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+					<path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+					<path fill-rule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clip-rule="evenodd" />
+				</svg>
+				
+				{:else}
+				<p>Private Collection</p>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+					<path d="M3.53 2.47a.75.75 0 00-1.06 1.06l18 18a.75.75 0 101.06-1.06l-18-18zM22.676 12.553a11.249 11.249 0 01-2.631 4.31l-3.099-3.099a5.25 5.25 0 00-6.71-6.71L7.759 4.577a11.217 11.217 0 014.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113z" />
+					<path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0115.75 12zM12.53 15.713l-4.243-4.244a3.75 3.75 0 004.243 4.243z" />
+					<path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 00-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 016.75 12z" />
+				</svg>
+				{/if}
 			</div>
-			<button on:click={() => dispatch('edit')} class='hover:opacity-70'>
+
+			<!-- edit button -->
+			<button on:click={showEditInput} class='hover:opacity-70'>
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-accent">
 					<path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
 				  </svg>
 				  
 			</button>
 
+			<!-- delete button -->
 			<button on:click={showDeleteCollectionConfirmation} class=" hover:opacity-70">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
