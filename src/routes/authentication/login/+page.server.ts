7a36b90b43b599d01bc298af4f32cbb0b7cc115b
haoens <seehaoen88@gmail.com>
2022-10-05 13:51:03 +1100

    Connected Login and Registration page to backend

--- src/routes/authentication/login/+page.server.ts
+++ src/routes/authentication/login/+page.server.ts
@@ -5,59 +5,17 @@ import { setCookie } from "lucia-sveltekit"
 // import { createNewEntity } from "../../database/mysql";
 
 export const actions: Actions = {
-	register: async ({ cookies, request }) => {
+
+	default: async ({ cookies, request, url }) => {
 		const data = await request.formData();
-		const username = data.get('username')?.toString() || '';
-		const password = data.get('password')?.toString() || '';
-		console.log(username, password);
-
-		try {
-			// Create user credentials, tokens and set cookies
-			const userSession = await auth.createUser('username', username, {
-				password,
-				user_data: {
-					name: username,
-					bio: '',
-					profilePic: '',
-					favAuthor: '',
-					favGenre: '',
-					isAuthor: false,
-					favBook: ''
-				}
-			});
-			setCookie(cookies, ...userSession.cookies);
-			console.log(userSession);
-
-			// Create user profile, which has username as the user's name in database
-			return { success: true };
-		} catch (err) {
-			const error = err as Error;
-			console.log(error.message);
-			throw redirect(303, '/authentication/failed');
-			// if (
-			//     error.message === "AUTH_INVALID_IDENTIFIER_TOKEN" ||
-			//     error.message === "AUTH_INVALID_PASSWORD"
-			// ) {
-			//     throw invalid(400, {
-			//         message: "Incorrect username or password",
-			//     });
-			// }
-			// // database connection error
-			// throw invalid(400, {
-			//     message: "Unknown error",
-			// });
-		}
-	},
-
-	login: async ({ cookies, request, url }) => {
-		const data = await request.formData();
-		const username = data.get('username')?.toString() || '';
+		const email = data.get('email')?.toString() || '';
 		const password = data.get('password')?.toString() || '';
 		try {
-			const userSession = await auth.authenticateUser('username', username, password);
+			const userSession = await auth.authenticateUser('email', email, password);
 			setCookie(cookies, ...userSession.cookies);
 			console.log('Successfully signed in');
 			console.log(url);
+			return { success: true };
 		} catch (e) {
 			const error = e as Error;
 			console.log(error.message);
