d5d671d6cbb7737a64cc94bda19c3e7f45c75b95
lucasvantilburg <78183150+lucasvantilburg@users.noreply.github.com>
2022-10-06 15:51:37 +1100

    IT 80: added functionality to edit reviews, collections and notes (#52)
    * connected delete review route
    
    * connected delete collection route
    
    * connected delete note route
    
    * removed todo comments
    
    * made UI for editing review
    
    * implemented UI for editing notes
    
    * created ui for editing collection, refactored collection input
    
    * removed unused vars

---
+++ src/components/BookPageComponents/Notes/CollectionInput.svelte
@@ -0,0 +1,122 @@
+<script lang='ts'>
+    import type { Collection } from 'src/types/book.type';
+    import { isOverlayOpen } from '../../../stores/OverlayStore.js';
+    import { page } from '$app/stores';
+	import { getSession } from 'lucia-sveltekit/client';
+	import { onMount, createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+	const session = getSession();
+	const user_id = $session?.user.user_id;
+
+	const bookId = $page.params.bookId;
+	const baseURL: string = $page.url.origin;
+
+    export let collection: Collection | undefined;
+
+    let collectionTitle = '';
+	let isPublic = false;
+
+    onMount(() => {
+        if (collection != undefined) {
+            collectionTitle = collection.title;
+            isPublic = collection.isPublic;
+        }
+    })
+
+    async function createNewCollection() {
+		//TODO: check string is not empty and add to database
+		// alert(`Title: ${collectionTitle}, isPublic: ${isPublic}`);
+
+		//remove overlay
+		isOverlayOpen.set(false);
+
+		const response = await fetch(`${baseURL}/api/create/collection/${bookId}`, {
+			method: 'POST',
+			body: JSON.stringify({
+				title: collectionTitle,
+				userId: user_id,
+				isPublic: isPublic
+			})
+		});
+
+		//reset
+		collectionTitle = '';
+		isPublic = false;
+
+        //bind to parent
+		collection = await response.json();
+
+		dispatch('newCollection');
+	}
+
+    function handleCancel() {
+        isOverlayOpen.set(false)
+        dispatch('cancel');
+    }
+
+    async function updateCollection() {
+        if (collection != undefined) {
+            //TODO: update in database
+
+            collection.title = collectionTitle;
+            collection.isPublic = isPublic;
+
+            isOverlayOpen.set(false);
+            dispatch('update');
+        }
+        
+    }
+
+</script>
+
+<div
+    class=" bg-white fixed z-10 top-1/2 left-1/2 w-96 flex flex-col p-3 rounded-2xl"
+    id="collectionForm"
+>
+    <h2 class=" text-secondary">New Notes Collection</h2>
+
+    <input
+        bind:value={collectionTitle}
+        type="text"
+        placeholder="Title..."
+        class=" standard_input my-2 bg-primary-1 rounded-full px-2 py-1"
+    />
+
+    <div class="flex justify-between text-body2 space-x-2 mt-1 w-full items-center">
+        <!-- TODO: replace with Flowbite toggle - wasn't working -->
+        <div class="flex items-center space-x-3 ml-1">
+            <label for="publicCheckbox" class="text-body1 font-body">Make Public</label>
+            <input
+                type="checkbox"
+                id="publicCheckbox"
+                bind:checked={isPublic}
+                class=" text-accent rounded-sm"
+            />
+        </div>
+
+        <div class="">
+            
+            <button
+                on:click={handleCancel}
+                class=" btn bg-primary-1 rounded-full px-2"><p>Cancel</p></button
+            >
+            {#if collection == undefined}
+            <button on:click={createNewCollection} class="btn bg-accent text-white rounded-full px-2"
+                ><p>Create</p></button
+            >
+            {:else}
+            <button on:click={updateCollection} class="btn bg-accent text-white rounded-full px-2"
+                ><p>Update</p></button
+            >
+            {/if}
+        </div>
+    </div>
+</div>
+
+<style>
+	#collectionForm {
+		transform: translate(-50%, -50%);
+	}
+</style>
--- src/components/BookPageComponents/Reviews/ReviewInput.svelte
+++ src/components/BookPageComponents/Reviews/ReviewInput.svelte
@@ -4,6 +4,10 @@
 	import { page } from '$app/stores';
 	import { getSession } from 'lucia-sveltekit/client';
 	import type { Review } from '../../../types/book.type';
+	import { onMount } from 'svelte';
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
 
 	const session = getSession();
 	const user_id = $session?.user.user_id;
@@ -17,6 +21,14 @@
 	let comment: string = '';
 
 	export let review: Review | undefined;
+
+	onMount(() => {
+		if (review !== undefined) {
+			rating = review.rating;
+			title = review.title;
+			comment = review.comment;
+		}
+	})
 
 	// async function getBookInfo(){
 	//     const response = await fetch(`${baseURL}/api/read/books/${bookId}/${client.user_id}`, )
@@ -48,6 +60,22 @@
 			alert(`missing required value`);
 		}
 	}
+
+	async function updateReview() {
+		if (review != undefined && rating != 0 && title != '' && comment != '') {
+			
+			//TODO: update review in database
+
+
+			review.rating = rating;
+			review.title = title;
+			review.comment = comment;
+			review.isEdited = true;
+
+			//tell Reviews Tab to go back to displaying ReviewCard
+			dispatch('cancel')
+		}
+	}
 </script>
 
 <div class=" flex flex-col space-y-2 text-body1 font-body">
@@ -60,6 +88,15 @@
 		class=" std_text_area"
 		placeholder="Write a review..."
 	/>
-
-	<button on:click={postReview} class=" std_button self-end">Post</button>
+	{#if review == undefined}
+		<!-- new review -->
+		<button on:click={postReview} class=" std_button self-end">Post</button>
+	{:else}
+		<!-- editting review -->
+		<div class="self-end">
+			<button on:click={() => dispatch('cancel')} class=" std_button bg-primary-1 text-primary-3">Cancel</button>
+			<button on:click={updateReview} class=" std_button">Update</button>
+		</div>
+		
+	{/if}
 </div>
