0ea1d43fe5147a663333a91055ba3e543ff3c049
Panagiotis Fatouros <pfatouros@student.unimelb.edu.au>
2022-10-12 11:23:06 +1100

    Move update into correct place.

--- src/routes/settings/+page.server.ts
+++ src/routes/settings/+page.server.ts
@@ -3,21 +3,29 @@ import { auth } from '$lib/lucia';
 import { redirect } from '@sveltejs/kit';
 import { prismaClient } from '../../lib/lucia';
 import type { Client } from '../../types/book.type';
+import { BlobServiceClient } from '@azure/storage-blob';
+
+// Create connection to blob storage
+let containerName = 'profile-photos';
+let AZURE_STORAGE_CONNECTION_STRING: string = 'DefaultEndpointsProtocol=https;AccountName=greatreadsblobstorage;AccountKey=b8DpLC3HoaRgip+rjzn2RcjRXw4fed4vEqTC6jRSQcFeu+HApYy9OjCtg3GHyuV0Fva49FAtvtEi+AStndok2g==;EndpointSuffix=core.windows.net';
+const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
+const containerClient = blobServiceClient.getContainerClient(containerName);
+
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ request }: ServerLoadEvent) {
-	try {
-		const session = await auth.validateRequestByCookie(request);
-		if (session) {
-			
+    try {
+        const session = await auth.validateRequestByCookie(request);
+        if (session) {
+
             const userID = session.user.user_id;
 
             const prismaUser = await prismaClient.user.findUnique({
-                where: {id: userID}
+                where: { id: userID }
             })
 
             if (prismaUser != undefined) {
-                const client:Client = {
+                const client: Client = {
                     name: prismaUser.name || '',
                     id: prismaUser.id || '',
                     profilePic: prismaUser.profilePic || '',
@@ -31,15 +39,22 @@ export async function load({ request }: ServerLoadEvent) {
                     client: client
                 };
             }
-            
 
-		} else {
-			//not authenticated
-			throw redirect(307, '/authentication');
-		}
-	} catch (err) {
-		console.log(err);
-		//not authenticated
-		throw redirect(307, '/authentication');
-	}
+
+        } else {
+            //not authenticated
+            throw redirect(307, '/authentication');
+        }
+    } catch (err) {
+        console.log(err);
+        //not authenticated
+        throw redirect(307, '/authentication');
+    }
+}
+
+async function uploadFileToStorage(file: string, fileExt: String, user: Client) {
+    let blobClient = containerClient.getBlockBlobClient(user.id + fileExt);
+    console.log(`\nUploading to Azure storage as blob\n\tname: ${user.id}:\n\tURL: ${blobClient.url}`);
+    let uploadResponse = await blobClient.upload(file, file.length);
+    console.log('\n Upload of image was successful: requestId: ${uploadResponse.requestId}');
 }
