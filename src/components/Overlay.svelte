7235df56be077779b7c52f17d5b78ecb4942e1fe
lucasvantilburg <78183150+lucasvantilburg@users.noreply.github.com>
2022-10-17 22:34:08 +1100

    IT-127: fixed error with creating bookshelf, fixed issue with overlay (#85)
    * fixed error with creating bookshelf, fixed issue with overlay
    
    * Update +layout.svelte

--- src/components/Overlay.svelte
+++ src/components/Overlay.svelte
@@ -2,6 +2,5 @@
 	import { isOverlayOpen } from "../stores/OverlayStore";
 </script> -->
 
-<div
-	class="w-screen h-screen fixed top-0 left-0 z-10 flex justify-center items-center bg-black opacity-80"
-/>
+<div class="w-full h-full fixed z-10 bg-black opacity-80">
+</div>
