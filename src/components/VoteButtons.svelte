34935ba30bfbefe23932175bc7224309becdc7a6
Luhuala <88075053+Luhuala@users.noreply.github.com>
2022-10-21 10:24:58 +1100

    Fixed vote buttons, added spaces to author lists (#98)

--- src/components/VoteButtons.svelte
+++ src/components/VoteButtons.svelte
@@ -1,35 +1,60 @@
 <script lang="ts">
-	//TODO:  - changing vote from up to down or vice versa should change count by 2 otherwise its just resetting to what it was before
-	//       - clicking same button again should undo vote
-	//       - pass in voteCount from parent component and dispatch event when changed
+	//TODO: pass in voteCount from parent component and dispatch event when changed
 
 	let colourUp: string = 'text-primary-3';
 	let colourDown: string = 'text-primary-3';
-	let hoverable: string = 'hover:text-accent';
+	let hoverUp: string = 'hover:text-accent';
+	let hoverDown: string = 'hover:text-accent';
 	let clickedUp: boolean = false;
 	let clickedDown: boolean = false;
 	let voteCount: number = 0; // get from database
 
 	// User can vote at most once in either direction, and can change their vote
 	function handleClickUp() {
-		if (!clickedUp) {
+		if (!clickedUp && !clickedDown) { // on first click
 			voteCount++;
+			colourUp = 'text-accent';
+			clickedUp = true;
+			clickedDown = false;
+			hoverUp = '';
+			hoverDown = 'hover:text-accent';
+		} else if (clickedUp && !clickedDown) { // already upvoted; undo upvote
+			voteCount--;
+			colourUp = 'text-primary-3';
+			clickedUp = false;
+			hoverUp = 'hover:text-accent';
+		} else if (!clickedUp && clickedDown) { // was downvoted, now upvoted
+			voteCount += 2;
 			colourUp = 'text-accent';
 			colourDown = 'text-primary-3';
 			clickedUp = true;
 			clickedDown = false;
+			hoverUp = '';
+			hoverDown = 'hover:text-accent';
 		}
-		hoverable = '';
 	}
 	function handleClickDown() {
-		if (!clickedDown) {
+		if (!clickedDown && !clickedUp) { // on first click
 			voteCount--;
 			colourDown = 'text-accent';
-			colourUp = 'text-primary-3';
-			clickedDown = true;
 			clickedUp = false;
+			clickedDown = true;
+			hoverUp = 'hover:text-accent';
+			hoverDown = '';
+		} else if (!clickedUp && clickedDown) { // already downvoted; undo downvote
+			voteCount++;
+			colourDown = 'text-primary-3';
+			clickedDown = false;
+			hoverDown = 'hover:text-accent';
+		} else if (clickedUp && !clickedDown) { // was upvoted, now downvoted
+			voteCount -= 2;
+			colourUp = 'text-primary-3';
+			colourDown = 'text-accent';
+			clickedUp = false;
+			clickedDown = true;
+			hoverUp = 'hover:text-accent';
+			hoverDown = '';
 		}
-		hoverable = '';
 	}
 </script>
 
@@ -42,7 +67,7 @@
 			viewBox="0 0 24 24"
 			stroke-width="2.5"
 			stroke="currentColor"
-			class="w-5 h-5 {colourUp} {hoverable}"
+			class="w-5 h-5 {colourUp} {hoverUp}"
 		>
 			<path
 				stroke-linecap="round"
@@ -62,7 +87,7 @@
 			viewBox="0 0 24 24"
 			stroke-width="2.5"
 			stroke="currentColor"
-			class="w-5 h-5 {colourDown} {hoverable}"
+			class="w-5 h-5 {colourDown} {hoverDown}"
 		>
 			<path
 				stroke-linecap="round"
