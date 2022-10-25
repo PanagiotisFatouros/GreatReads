62232f4fc2ef34a44ee2acb1afdc2e1ef24ad9b9
yuqi-zheng <yuqizheng02@gmail.com>
2022-10-25 21:51:17 +1100

    added search bar to unauthenticated user

--- src/components/NavBarLanding.svelte
+++ src/components/NavBarLanding.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { isOverlayOpen } from "../stores/OverlayStore"
     import { page } from "$app/stores";
+    import HeaderSearchBar from "./HeaderSearchBar.svelte";
 
     function openOverlay() {
         isOverlayOpen.set(true)
@@ -19,10 +20,13 @@
 
 <div id="navBar" class="{landing ? 'bg-primary-1 bg-opacity-25': 'bg-primary-3'} w-full h-14  flex justify-between content-center m-0">
     <a id="logo" href={'/authentication'} class="self-center p-4 font-logo text-primary-1 text-2xl">GreatReads</a>
+    {#if !landing}
+        <HeaderSearchBar/>
+    {/if}
     <div id="rightNav" class="flex justify-around content-center">
         <a href="/authentication/login" on:click={openOverlay}><p class="list-none p-3 text-primary-1 font-heading text-lg font-300">Log In</p></a>
         <p class="list-none p-3 text-primary-1 font-heading text-lg font-300">|</p>
-        <a href="/authentication/register"><p class="list-none p-3 text-primary-1 font-heading text-lg font-300">Register</p></a>
+        <a href="/authentication/register"><p class="list-none p-3 text-primary-1 font-heading text-lg font-300">Register</p></a>     
     </div>
 </div>
 
