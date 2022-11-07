8f568eee12af456bc2f8ae09fdcfdcf8639fb64e
haoens <seehaoen88@gmail.com>
2022-11-07 18:44:11 +1100

    page.navigation test

--- tests/library.test.js
+++ tests/library.test.js
@@ -48,6 +48,7 @@ test('Add book to & Remove book from bookshelves', async ({ page, baseURL }) => 
     // await page.waitForSelector('#save > div.flex.flex-col.w-full.space-y-3.mt-3 > label:has-text("Favourites")');
     // await page.locator('#save > div.flex.flex-col.w-full.space-y-3.mt-3 > label:has-text("Favourites")').click();
     // await page.locator('#save > div.mt-3.self-end.space-x-2 > button.btn.bg-accent.text-white.rounded-full.px-4.py-1').click();
+    await page.waitForNavigation({waitUntil: "domcontentloaded"});
     await page.locator('button', {hasText: "Saved"}).click();
     await page.locator('label', {hasText: "Favourites"}).click();
     await page.locator('button', {hasText: "Confirm"}).click();
