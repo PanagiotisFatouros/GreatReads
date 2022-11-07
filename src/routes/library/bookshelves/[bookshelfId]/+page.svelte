f6f1194ca9a7c6eb7c5f0a74bc4f0c483b8314f7
lucasvantilburg <78183150+lucasvantilburg@users.noreply.github.com>
2022-11-07 22:26:38 +1100

    IT-139: implemented editing bookshelf name (#118)
    * implemented editing bookshelf name
    
    * fixed font colour

--- src/routes/library/bookshelves/[bookshelfId]/+page.svelte
+++ src/routes/library/bookshelves/[bookshelfId]/+page.svelte
@@ -2,6 +2,7 @@
 	import BookCard from '../../../../components/BookCard.svelte';
 	import FilterPanel from '../../../../components/FilterPanel.svelte';
 	import SortPanel from '../../../../components/SortPanel.svelte';
+	import AddBookshelfPanel from '../../../../components/AddBookshelfPanel.svelte';
 	import { isOverlayOpen } from '../../../../stores/OverlayStore.js';
 	import type { Bookshelf, Book } from '../../../../types/book.type';
 
@@ -9,6 +10,7 @@
 	import Confirmation from '../../../../components/Confirmation.svelte';
 	import { page } from '$app/stores';
 	import { getSession } from 'lucia-sveltekit/client';
+	
 
 	const session = getSession();
 
@@ -26,7 +28,8 @@
 	let sortOn = false;
 
 	let isDeleting:boolean = false;
-	
+	let isEditing:boolean = false;
+
 	function handleMaybeRemoved(event:any, bookId: string) {
 	
 		if (event.detail.bookshelfIDs.includes(bookshelf.id)) {
@@ -46,7 +49,7 @@
 		goto('/library/bookshelves');
 	}
 
-	$: isOverlayOpen.set(filterOn || sortOn || isDeleting);
+	$: isOverlayOpen.set(filterOn || sortOn || isDeleting || isEditing);
 	$: bookshelf
 	$: booksShown
 	$: {
@@ -54,6 +57,7 @@
 			filterOn = false;
 			sortOn = false;
 			isDeleting = false;
+			isEditing = false;
 		}
 	}
 
@@ -126,7 +130,18 @@
 		</div>
 
 		{#if $session && bookshelf.isDeletable}
-		<button on:click={() => isDeleting = true} class="flex text-accent ml-5">
+		<button on:click={() => isEditing = true} class="flex text-primary-3 ml-2">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+				<path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+			</svg>
+			  
+			  
+			<p class="ml-1 mr-3">Change Name</p>
+		</button>
+		{/if}
+
+		{#if $session && bookshelf.isDeletable}
+		<button on:click={() => isDeleting = true} class="flex text-accent ml-2">
 			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 				<path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
 			</svg>
@@ -152,12 +167,17 @@
 	{#if isDeleting}
 		<Confirmation title={"Delete Bookshelf"} description={"Are you sure you want to delete this bookshelf? <br/>This can not be undone."} on:cancel={() => isDeleting = false} on:confirm={deleteBookshelf}/>
 	{/if}
+	{#if isEditing}
+		<AddBookshelfPanel bind:show={isEditing} bind:bookshelf={bookshelf} />
+	{/if}
 </div>
 
 <div class="mx-6 flex flex-row flex-wrap grow justify-items-center items-center">
-	{#if booksShown != undefined}
+	{#if booksShown != undefined && booksShown.length > 0}
 		{#each booksShown as book}
 			<BookCard on:click={() => goto(`/books/${book.id}`)} on:maybeRemoved={event => handleMaybeRemoved(event, book.id)} book={book} bookshelves={allBookshelves} />
 		{/each}
+	{:else}
+	<h2 class=" text-primary-3 ml-3 mt-5">No Books Found</h2>
 	{/if}
 </div>
