a22c723732afbd2d6e0e8b0955df20b8883d0f41
haoens <seehaoen88@gmail.com>
2022-11-07 18:28:39 +1100

    modify locator

--- tests/library.test.js
+++ tests/library.test.js
@@ -48,9 +48,9 @@ test('Add book to & Remove book from bookshelves', async ({ page, baseURL }) => 
     // await page.waitForSelector('#save > div.flex.flex-col.w-full.space-y-3.mt-3 > label:has-text("Favourites")');
     // await page.locator('#save > div.flex.flex-col.w-full.space-y-3.mt-3 > label:has-text("Favourites")').click();
     // await page.locator('#save > div.mt-3.self-end.space-x-2 > button.btn.bg-accent.text-white.rounded-full.px-4.py-1').click();
-    await page.locator('button:has-text("Saved")').click();
-    await page.locator('label:has-text("Favourites")').click();
-    await page.locator('button:has-text("Confirm")').click();
+    await page.locator('button', {hasText: "Saved"}).click();
+    await page.locator('label', {hasText: "Favourites"}).click();
+    await page.locator('button', {hasText: "Confirm"}).click();
     
     // Back To Home Page, Check no more book in favourites
     console.log(page.url())
