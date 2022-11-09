8901f9ba1a4d12d3fe8b960e7d064a12b8aef068
Panagiotis Fatouros <86960265+PanagiotisFatouros@users.noreply.github.com>
2022-11-09 15:15:44 +1100

    IT164: Move to environment variables. Azure has been updated accordingly. (#124)
    * Put database connection string in .env
    
    * Changed Lucia secret to be a environment variables
    
    * Switch to dev database for local development.
    
    * Fix mistake in lucia file

--- src/lib/lucia.js
+++ src/lib/lucia.js
@@ -6,6 +6,6 @@ export const prismaClient = new PrismaClient();
 
 export const auth = lucia({
 	adapter: prisma(prismaClient),
-	secret: ')J@NcRfUjXn2r5u7x!A%D*G-KaPdSgVk',
-	env: (process.env.NODE_ENV === 'production') ? "DEV" : "PROD",
+	secret: process.env.LUCIA_SECRET,
+	env: (process.env.NODE_ENV === 'production') ? "PROD" : "DEV",
 });
